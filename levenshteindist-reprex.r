51a7ff01beb2199f4188a305b2f6b54d8a5ca466
Panos Mavros <pmavros@users.noreply.github.com>
2023-04-03 14:51:20 +0200

    Create levenshteindist-reprex.r

---
+++ levenshteindist-reprex.r
@@ -0,0 +1,11 @@
+stringdist(a = "abcdef", "afbcde", method = "lv")
+# evals: 2
+
+stringdist(a = "abcdef", "adfbce", method = "lv")
+# evals: 4
+
+stringdist(a = "abcdef", "afbced", method = "lv")
+# evals: 3
+
+stringdist(a = "adgicejhf", "ajhecigdf", method = "lv")
+# evals: 6
